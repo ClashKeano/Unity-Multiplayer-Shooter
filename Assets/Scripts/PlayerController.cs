c64cfad6488d110eb0c7e74da6264ea86bfb3ce5
Aidan <aidankeane93@gmail.com>
2022-07-11 12:03:24 +0200

    First commit, basic player model and environment

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform viewPoint;
+    public float mouseSens = 1f;
+    private float vertRotation;
+    private Vector2 mouseInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    } 
+}
